08460a8298870165da175333782d34224d84c5b6
breadworshipper <azhrayashna@gmail.com>
2024-10-07 15:42:27 +0700

    FEAT: Added user repository

--- internal/module/user/repository/repo.go
+++ internal/module/user/repository/repo.go
@@ -1,17 +1,42 @@
 package repository
 
 import (
-	"github.com/jmoiron/sqlx"
+	"pddikti-cms/internal/module/user/entity"
+	"pddikti-cms/internal/module/user/ports"
+
+	"gorm.io/gorm"
 )
 
-var _ ports.XxxRepository = &xxxRepository{}
+var _ ports.UserRepository = &userRepository{}
 
-type xxxRepository struct {
-	db *sqlx.DB
+type userRepository struct {
+	db *gorm.DB
 }
 
-func NewXxxRepository(db *sqlx.DB) *xxxRepository {
-	return &xxxRepository{
+func NewUserRepository(db *gorm.DB) *userRepository {
+	return &userRepository{
 		db: db,
 	}
+}
+
+func (r *userRepository) EmailExist(email string) bool {
+	var count int64
+	r.db.Model(&entity.User{}).Where("email = ?", email).Count(&count)
+	return count > 0
+}
+
+func (r *userRepository) Register(user *entity.User) error {
+	return r.db.Create(user).Error
+}
+
+func (r *userRepository) GetUserByEmail(email string) (*entity.User, error) {
+	var user entity.User
+	err := r.db.Where("email = ?", email).First(&user).Error
+	return &user, err
+}
+
+func (r *userRepository) GetUserById(id string) (*entity.User, error) {
+	var user entity.User
+	err := r.db.Where("id = ?", id).First(&user).Error
+	return &user, err
 }
