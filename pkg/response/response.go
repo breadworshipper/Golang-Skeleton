e53c76c7292dee13284edff59a3db954f6489287
azizikri <azizikri@gmail.com>
2024-10-08 20:28:42 +0700

    FEAT: Refactor response handling to use structured types for success and error responses

--- pkg/response/response.go
+++ pkg/response/response.go
@@ -1,72 +1,86 @@
 package response
 
-import "mm-pddikti-cms/pkg/errmsg"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
-type Response map[string]any
-
-func Success(data any, message string) Response {
-	// msg := "Your request has been successfully processed"
-	msg := "Permintaan Anda berhasil diproses"
-	if message != "" {
-		msg = message
-	}
-	return Response{
-		"success": true,
-		"message": msg,
-		"data":    data,
-	}
+// Typed structures for common responses
+type SuccessResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message,omitempty"`
+	Data    any    `json:"data,omitempty"`
 }
 
-func Error(errorMsg any) Response {
-	if _, ok := errorMsg.(string); ok {
-		return Response{
-			"errors":  make(map[string][]string),
-			"success": false,
-			"message": errorMsg,
-		}
-	}
-
-	if _, ok := errorMsg.(map[string][]string); ok {
-		return Response{
-			"success": false,
-			"errors":  errorMsg,
-			// "message": "Your request has been failed to process",
-			"message": "Permintaan Anda gagal diproses",
-		}
-	}
-
-	if errHttp, ok := errorMsg.(*errmsg.CustomError); ok {
-		return Response{
-			"errors":  errHttp.Errors,
-			"success": false,
-			"message": errHttp.Msg,
-		}
-	}
-
-	if err, ok := errorMsg.(error); ok {
-		return Response{
-			"errors":  make(map[string][]string),
-			"success": false,
-			"message": err.Error(),
-		}
-	}
-
-	return Response{
-		"success": false,
-		// "message": "Your request has been failed to process",
-		"message": "Permintaan Anda gagal diproses",
-	}
+type ErrorValidation struct {
+	Error       bool
+	FailedField string
+	Tag         string
+	Value       interface{}
 }
 
-func ErrorsWithMessage(errors map[string][]string, message string) Response {
-	if message == "" {
-		// message = "Your request has been failed to process"
-		message = "Permintaan Anda gagal diproses"
+type ErrorResponse struct {
+	Status  string            `json:"status"`
+	Message string            `json:"message,omitempty"`
+	Errors  []ErrorValidation `json:"errors"`
+}
+
+type PaginatedResponse struct {
+	Status   string      `json:"status"`
+	Message  string      `json:"message,omitempty"`
+	Data     any         `json:"data,omitempty"`
+	Paginate *Pagination `json:"pagination,omitempty"`
+}
+
+type Pagination struct {
+	Page      int `json:"page"`
+	PerPage   int `json:"per_page"`
+	Total     int `json:"total"`
+	TotalPage int `json:"total_page"`
+}
+
+type ResponseParams struct {
+	StatusCode int
+	Message    string
+	Paginate   *Pagination
+	Data       any
+	Errors     []ErrorValidation
+}
+
+func SendResponse(ctx *fiber.Ctx, params ResponseParams) error {
+	var response interface{}
+	var status string
+
+	if params.StatusCode >= 200 && params.StatusCode <= 299 {
+		status = "success"
+	} else {
+		status = "error"
 	}
 
-	return Response{
-		"errors":  errors,
-		"success": false,
-		"message": message,
+	if params.Data != nil && params.Paginate == nil {
+		response = &SuccessResponse{
+			Status:  status,
+			Message: params.Message,
+			Data:    params.Data,
+		}
+	} else if params.Data != nil && params.Paginate != nil {
+		response = &PaginatedResponse{
+			Status:   status,
+			Message:  params.Message,
+			Data:     params.Data,
+			Paginate: params.Paginate,
+		}
+	} else if len(params.Errors) > 0 {
+		response = &ErrorResponse{
+			Status:  status,
+			Message: params.Message,
+			Errors:  params.Errors,
+		}
+	} else {
+		response = map[string]interface{}{
+			"status":  status,
+			"message": params.Message,
+		}
 	}
+
+	return ctx.Status(params.StatusCode).JSON(response)
 }
